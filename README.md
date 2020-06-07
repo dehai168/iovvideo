3b5a337821c14665d437f0a448e2dd28875138c5
dhwang-streamax <dhwang@streamax.com>
2020-06-07 17:50:04 +0800

    更新

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# iovvideo
-h5 video，support flv,hls,   support h264,aac
+###  H5简易播放器说明
+
+#### 技术栈
+
+1. flv.js(1.5.0)
+2. jquery(3.4.1)
+3. flash (as3)
+4. ffmpeg
+5. node-media-server
+
+### 文件目录结构
+
+1. document     (as3 语法文档)
+2. ffmpeg       (ffmpeg windows版本build exe)
+3. flash        (flash 播放器源码,内部使用rtmp方式打开视频)
+4. node_modules (npm 包文件)
+5. public       (嵌入页面文件夹)
+   --- index_h5.html (flv方式h5页面)
+   --- index.html    (flash方式页面)
+6. app.js       程序服务启动 app.js
+7. config.js    程序配置文件
+
+### 使用方法
+
+1. npm i
+2. 修改 `config.js` `public/index_h5.html(670行)`  配置文件
+3. 运行 `node ./app.js`  (或者用nssm注册为服务)
+4. 打开浏览器访问 `http://127.0.0.1:8000/admin` 可以管理媒体服务器地址
+5. 打开浏览器访问 `http://127.0.0.1:80/index_h5.html` 为h5的播放页面
+
+### 业务平台使用播放页面方法
+
+1. 方法1:在需要打开视频的页面通过打开浏览器新Tab页面的方式打开视频即调用:
+    ```js
+    window.open('/public/index_h5.html?xxxxxx','_blank')
+    ```
+2. 方法2:在需要打开视频的页面通过嵌入ifream的方式进行调用 
+    ```html
+    <ifream src="/public/index_h5.html?xxxxxx"></ifream>
+    ```
+### 播放页面参数说明
+
+key | 说明  |  备注  
+-|-|-
+channel|通道数|1,2,3,4
+mediaurl|809企业视频地址|http://[ip]:[port]/[车牌号码].[车牌颜色].[逻辑通道号].[音视频标志(0:音视频,1:音频,2:视频)].[时效口令],该参数只获取逻辑通道1的即可，其他通道由当前页面进行计算,对该url进行参数编码
+serverip|媒体服务器地址|部署服务器ip
+httpport|请求rtmp前需要请求http端口,可以不传|默认12345
+rtmpport|rtmp端口,可以不传|默认1935
+
+
+### 注意事项
+
+1. 保证外网访问的端口都可以访问,通过配置防火墙
+3. 由于浏览器有并发下载限制，所以媒体服务默认配置了3组服务器满足18路视频并发.
+4. 由于参数中有多个.导致参数解析问题,需要修改 node-media-server/node_flv_session.js  63行内 split('.')=>split('.flv')
+5. 参考  https://github.com/illuspas/Node-Media-Server/blob/master/README_CN.md 
--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const NodeMediaServer = require('node-media-server');
-//可用rtmp地址
-//香港财经,rtmp://202.69.69.180:443/webcast/bshdlive-pc
-//韩国朝鲜日报: rtmp://live.chosun.gscdn.com/live/tvchosun1.stream
-//美国1,rtmp://ns8.indexforce.com/home/mystream
-//美国2,rtmp://media3.scctv.net/live/scctv_800
-//美国中文电视,rtmp://media3.sinovision.net:1935/live/livestream
-//湖南卫视,rtmp://58.200.131.2:1935/livetv/hunantv
+const express = require('express');
+const app = express();
+const appConfig = require('./config');
+
+//server
 const config = {
     rtmp: {
         port: 1935,
@@ -14,26 +12,43 @@ const config = {
         ping: 30,
         ping_timeout: 60
     },
+    // http方式flv或websocket方式flv
     http: {
-        port: 8000,
+        port: appConfig.h5port1,
         allow_origin: '*'
     },
+    // rtmp中继地址
     relay: {
         ffmpeg: 'ffmpeg/bin/ffmpeg.exe',
-        tasks: [{
-            app: 'iptv',
-            mode: 'static',
-            edge: 'rtmp://202.69.69.180:443/webcast/bshdlive-pc',
-            name: 'hks'
-        }, {
-            app: 'mv',
-            mode: 'static',
-            edge: 'xhssf.mp4',
-            name: 'dq'
-        }
+        tasks: [
+            {
+                app: 'live',
+                mode: 'pull',
+                edge: appConfig.rtmpedge,
+            }
         ]
     }
 };
+// rtmp://58.200.131.2:1935/livetv/cctv1
+// rtmp://58.200.131.2:1935/livetv/cctv2
+// server node1
+var nms1 = new NodeMediaServer(config)
+nms1.run();
+// server node2
+var config2 = config;
+config2.rtmp.port++;
+config2.http.port = appConfig.h5port2;
+var nms2 = new NodeMediaServer(config2);
+nms2.run();
+// server node3
+var config3 = config2;
+config3.rtmp.port++;
+config3.http.port = appConfig.h5port3;
+var nms3 = new NodeMediaServer(config3);
+nms3.run();
 
-var nms = new NodeMediaServer(config)
-nms.run();
+// web
+app.use(express.static('public'))
+app.get('/', (req, res) => res.redirect('index.html'))
+app.get('/live', (req, res) => res.json({ code: 200, msg: 'ok' }))
+app.listen(appConfig.webport, () => console.info('Example app listening on port ' + appConfig.webport + '!'))
